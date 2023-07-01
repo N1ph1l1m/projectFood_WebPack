1c311463903df4f73d05a05f77fd04ea0239da60
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2023-07-01 15:22:07 +0300

    Add Babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,39 @@
-'use strict';
+"use strict";
 
-let path = require('path');
+let path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/js/script.js',
+  mode: "development",
+  entry: "./src/js/script.js",
   output: {
-    path: path.resolve(__dirname, 'dist/js'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist/js"),
+    filename: "bundle.js",
   },
   watch: true,
 
   devtool: "source-map",
 
-  module: {}
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  debug: true,
+                  corejs: 3,
+                  useBuiltIns: "usage",
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
 };
